47a9d8b4e4d55ef45b4aedc3cdb57d01d9df29bc
manju2367 <neco2367@outlook.jp>
2026-09-19 02:00:35 +0900

    feat: 起動確認

---
+++ electron/main.ts
@@ -0,0 +1,43 @@
+import { app, BrowserWindow } from 'electron'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const title = 'Mayo'
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        title,
+        width: 960,
+        height: 640,
+        autoHideMenuBar: true
+    })
+
+    if (app.isPackaged) {
+        mainWindow.loadFile(path.join(__dirname, 'index.html'))
+    } else {
+        mainWindow.loadURL('http://localhost:5173/')
+    }
+}
+
+
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
